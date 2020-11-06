2d26f93e9ee34ad2614d3d34d1e1baf207dea2a8
LasMouches <natures@hotmail.fr>
2020-11-06 17:03:43 +0100

    Animation et BoundingBox

--- src/resources/scripts/Player.cs
+++ src/resources/scripts/Player.cs
@@ -2,24 +2,12 @@ using Godot;
 using System;
 using Moonshot2020.resources.scripts;
 
-public enum HorizontalMovement
-{
-    Left = -1,
-    None = 0,
-    Right = 1
-}
-public enum VerticalMovement
-{
-    Up = -1,
-    None = 0,
-    Down = 1
-}
 public class Player : KinematicBody2D
 {
-    private HorizontalMovement _inputHorizontalDirection;
-    private VerticalMovement _inputVerticalDirection;
+	private HorizontalMovement _inputHorizontalDirection;
+	private VerticalMovement _inputVerticalDirection;
 
-    #region Constants
+	#region Constants
 
 	public float MaxRunSpeed { get; } = 250;
 	public float Acceleration { get; } = 50;
@@ -30,6 +18,7 @@ public class Player : KinematicBody2D
 
 	public AnimationPlayer Animation => GetNode<AnimationPlayer>("PlayerAnimation");
 	public Sprite Sprite => GetNode<Sprite>("PlayerSprite");
+	public CollisionShape2D BoundingBox => GetNode<CollisionShape2D>("BoundingBox");
 	public Vector2 Velocity { get; set; }
 
 
@@ -43,83 +32,86 @@ public class Player : KinematicBody2D
 
 	public override void _PhysicsProcess(float delta)
 	{
-        bool canMove = CanMove();
-        if (canMove)
-            HandleMovements();
+		bool canMove = CanMove();
+		if (canMove)
+			HandleMovements();
 
-        Velocity = MoveAndSlide(Velocity);
-        //if (_inputHorizontalDirection != HorizontalMovement.None || _inputVerticalDirection != VerticalMovement.None)
-        //	PlayAnimation("Run");
-        //else
-        //    PlayAnimation("Idle");
-    }
-
-
-    #region Move
-
-    private bool CanMove()
-    {
-        return true;
-    }
-
-    private void HandleMovements()
-    {
-        float horizontalVelocity;
-        float verticalVelocity;
-
-        float LimitSpeed(float speed)
-        {
-            return Mathf.Min(Mathf.Max(speed, -MaxRunSpeed), MaxRunSpeed);
-        }
-
-        _inputHorizontalDirection = (HorizontalMovement)Convert.ToInt32(Input.IsActionPressed(Inputs.MoveRight)) - Convert.ToInt32(Input.IsActionPressed(Inputs.MoveLeft));
-        _inputVerticalDirection = (VerticalMovement)Convert.ToInt32(Input.IsActionPressed(Inputs.MoveDown)) - Convert.ToInt32(Input.IsActionPressed(Inputs.MoveUp));
-
-        if (_inputHorizontalDirection == HorizontalMovement.Left && Velocity.x > -MaxRunSpeed)
-        {
-            horizontalVelocity = LimitSpeed(Velocity.x - Acceleration);
-            SetPlayerDirection(HorizontalMovement.Left);
-        }
-        else if (_inputHorizontalDirection == HorizontalMovement.Right && Velocity.x < MaxRunSpeed)
-        {
-            horizontalVelocity = LimitSpeed(Velocity.x + Acceleration);
-            SetPlayerDirection(HorizontalMovement.Right);
-        }
-        else
-        {
-            horizontalVelocity = Mathf.Abs(Velocity.x) <= 1 ? 0 : Mathf.Lerp(Velocity.x, 0, Inertia);
-        }
-        
-        if (_inputVerticalDirection == VerticalMovement.Up && Velocity.y > -MaxRunSpeed)
-        {
-            verticalVelocity = LimitSpeed(Velocity.y - Acceleration);
-        }
-        else if (_inputVerticalDirection == VerticalMovement.Down && Velocity.y < MaxRunSpeed)
-        {
-            verticalVelocity = LimitSpeed(Velocity.y + Acceleration);
-        }
-        else
-        {
-            verticalVelocity = Mathf.Abs(Velocity.y) <= 1 ? 0 : Mathf.Lerp(Velocity.y, 0, Inertia);
-        }
-        Velocity = new Vector2(horizontalVelocity, verticalVelocity);
-    }
-
-    #endregion
-
-    public void PlayAnimation(string animation)
-	{
-		if (Animation.CurrentAnimation == animation) return;
-        Animation.Play(animation);
+		Velocity = MoveAndSlide(Velocity);
+		if (_inputHorizontalDirection != HorizontalMovement.None || _inputVerticalDirection != VerticalMovement.None)
+			PlayAnimation("Run");
+		else
+			PlayAnimation("Idle");
 	}
 
-    private HorizontalMovement _currentDirection;
-    public void SetPlayerDirection(HorizontalMovement direction)
-    {
-        if (direction != HorizontalMovement.None && direction != _currentDirection)
-        {
-            Sprite.FlipH = direction == HorizontalMovement.Left;
-        }
-        //PlayerAnimation. = _playerDirection == HorizontalMovement.Left;
-    }
+
+	#region Move
+
+	private bool CanMove()
+	{
+		return true;
+	}
+
+	private void HandleMovements()
+	{
+		float horizontalVelocity;
+		float verticalVelocity;
+
+		float LimitSpeed(float speed)
+		{
+			return Mathf.Min(Mathf.Max(speed, -MaxRunSpeed), MaxRunSpeed);
+		}
+
+		_inputHorizontalDirection = (HorizontalMovement)Convert.ToInt32(Input.IsActionPressed(Inputs.MoveRight)) - Convert.ToInt32(Input.IsActionPressed(Inputs.MoveLeft));
+		_inputVerticalDirection = (VerticalMovement)Convert.ToInt32(Input.IsActionPressed(Inputs.MoveDown)) - Convert.ToInt32(Input.IsActionPressed(Inputs.MoveUp));
+
+		if (_inputHorizontalDirection == HorizontalMovement.Left && Velocity.x > -MaxRunSpeed)
+		{
+			horizontalVelocity = LimitSpeed(Velocity.x - Acceleration);
+			SetPlayerDirection(HorizontalMovement.Left);
+		}
+		else if (_inputHorizontalDirection == HorizontalMovement.Right && Velocity.x < MaxRunSpeed)
+		{
+			horizontalVelocity = LimitSpeed(Velocity.x + Acceleration);
+			SetPlayerDirection(HorizontalMovement.Right);
+		}
+		else
+		{
+			horizontalVelocity = Mathf.Abs(Velocity.x) <= 1 ? 0 : Mathf.Lerp(Velocity.x, 0, Inertia);
+		}
+		
+		if (_inputVerticalDirection == VerticalMovement.Up && Velocity.y > -MaxRunSpeed)
+		{
+			verticalVelocity = LimitSpeed(Velocity.y - Acceleration);
+		}
+		else if (_inputVerticalDirection == VerticalMovement.Down && Velocity.y < MaxRunSpeed)
+		{
+			verticalVelocity = LimitSpeed(Velocity.y + Acceleration);
+		}
+		else
+		{
+			verticalVelocity = Mathf.Abs(Velocity.y) <= 1 ? 0 : Mathf.Lerp(Velocity.y, 0, Inertia);
+		}
+		Velocity = new Vector2(horizontalVelocity, verticalVelocity);
+	}
+
+	#endregion
+
+	public void PlayAnimation(string animation)
+	{
+		if (Animation.CurrentAnimation == animation)
+			return;
+		Animation.Play(animation);
+	}
+
+	private HorizontalMovement _currentDirection = HorizontalMovement.None;
+	public void SetPlayerDirection(HorizontalMovement direction)
+	{
+		if (direction != HorizontalMovement.None && direction != _currentDirection)
+		{
+			_currentDirection = direction;
+			Sprite.FlipH = direction == HorizontalMovement.Left;
+			Sprite.Offset = new Vector2((int)direction * 9, Sprite.Offset.y);
+		}
+		//PlayerAnimation. = _playerDirection == HorizontalMovement.Left;
+	}
 }
